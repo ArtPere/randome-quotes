de97454875578fad5cef223d66a7ffd8bc11ab46
Artiom <timek3@gmail.com>
2025-08-12 18:58:59 +0200

    Selecting qouts from array of qoutes when the button is clicked

---
+++ index.js
@@ -0,0 +1,8 @@
+// import RandomQuotesApp from "./src/classes/RandomQuotesApp.js";
+
+// new RandomQuotesApp();
+
+const gBtn = document.getElementById("random-quote-btn");
+generateBtn.addEventListener("click", () => {
+  console.log("Button clicked!");
+});
