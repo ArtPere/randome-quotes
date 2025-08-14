e9b71f92927f7b2f30ff91a39eb48f8934f0a582
Artiom <timek3@gmail.com>
2025-08-14 18:00:02 +0200

    Adding and removing

--- index.js
+++ index.js
@@ -1,8 +1,46 @@
-// import RandomQuotesApp from "./src/classes/RandomQuotesApp.js";
+import quotes from "./quotes.js";
 
-// new RandomQuotesApp();
+const quoteElement = document.getElementById("quote");
+const authorElement = document.getElementById("quote-author");
+const generateBtn = document.getElementById("generate-btn");
+const togglFavoriteBtn = document.getElementById("toggle-favorite-btn");
+const favoritesContainer = document.getElementById("favorites-container");
+let currentQuoteIndex = null;
+console.log(currentQuoteIndex);
+function generateRandomQuote() {
+  currentQuoteIndex = Math.floor(Math.random() * quotes.length);
+  const currentQuotes = quotes[currentQuoteIndex];
+  const { text, author } = currentQuotes;
+  console.log(currentQuoteIndex);
+  togglFavoriteBtn.textContent = quotes[currentQuoteIndex].isFavorite
+    ? "Remove from favorites"
+    : "Add to favorites";
 
-const gBtn = document.getElementById("random-quote-btn");
-generateBtn.addEventListener("click", () => {
-  console.log("Button clicked!");
-});
+  quoteElement.textContent = text;
+  authorElement.textContent = author;
+}
+function toggLeFavorite() {
+  quotes[currentQuoteIndex].isFavorite = !quotes[currentQuoteIndex].isFavorite;
+  togglFavoriteBtn.textContent = quotes[currentQuoteIndex].isFavorite
+    ? "Remove from favorites"
+    : "Add to favorites";
+
+  if (quotes[currentQuoteIndex].isFavorite) {
+    const favoriteCard = document.createElement("div");
+    favoriteCard.classList.add("favorite-card");
+    favoriteCard.innerHTML = `
+        <p class="favorite-quote">${quotes[currentQuoteIndex].text}</p>
+        <p class="favorite-author">${quotes[currentQuoteIndex].author}</p>
+      `;
+    favoritesContainer.appendChild(favoriteCard);
+  } else {
+    const favoriteCards = document.querySelectorAll(".favorite-card");
+    favoriteCards.forEach((card) => {
+      if (card.textContent.includes(quotes[currentQuoteIndex].text)) {
+        card.remove();
+      }
+    });
+  }
+}
+generateBtn.addEventListener("click", generateRandomQuote);
+togglFavoriteBtn.addEventListener("click", toggLeFavorite);
